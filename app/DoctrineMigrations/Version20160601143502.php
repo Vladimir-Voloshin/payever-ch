4b20c92efa5d7391a255b0a58392ae1ec7d5e6ff
Boris Doomer <doomer@localhost.localdomain>
2016-06-01 23:42:03 +0300

    misc

--- app/DoctrineMigrations/Version20160601143502.php
+++ app/DoctrineMigrations/Version20160601143502.php
@@ -18,17 +18,8 @@ class Version20160601143502 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE images (
-            id INT AUTO_INCREMENT NOT NULL, 
-            album_id INT DEFAULT NULL, 
-            image_name VARCHAR(255) NOT NULL, 
-            created DATETIME NOT NULL, 
-            updated DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_E01FBE6AAC199498 (image_name), INDEX IDX_E01FBE6A1137ABCF (album_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE albums (
-            id INT AUTO_INCREMENT NOT NULL, 
-            album_name VARCHAR(255) NOT NULL, 
-            created DATETIME NOT NULL, 
-            updated DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE images (id INT AUTO_INCREMENT NOT NULL, album_id INT DEFAULT NULL, image_name VARCHAR(255) NOT NULL, created DATETIME NOT NULL, updated DATETIME DEFAULT NULL, INDEX IDX_E01FBE6A1137ABCF (album_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE albums (id INT AUTO_INCREMENT NOT NULL, album_name VARCHAR(255) NOT NULL, created DATETIME NOT NULL, updated DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE images ADD CONSTRAINT FK_E01FBE6A1137ABCF FOREIGN KEY (album_id) REFERENCES albums (id)');
     }
 
